27b313cf6e8fb0aac9d99eb5bb975e73ab145bda
Chris Gonzales <cm.gonzales@yahoo.com>
2018-06-13 17:51:23 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Nav from '.nav.jsx'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <Nav/>
+        
+       
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/nav.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class nav extends Component {
+  render() {
+    return (
+      <div>
+        <h1>the nav</h1>
+     
+      </div>
+    );
+  }
+}
+
+export default Nav;
