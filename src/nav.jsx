53338f670d3189f53c5eb49ece697f506e52e88f
Chris Gonzales <cm.gonzales@yahoo.com>
2018-06-14 18:24:02 -0700

    added nav from bootrap

--- src/nav.jsx
+++ src/nav.jsx
@@ -1,15 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Nav, NavItem, Dropdown, DropdownItem, DropdownToggle, DropdownMenu, NavLink } from 'reactstrap';
 
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
 
-class nav extends Component {
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
   render() {
     return (
       <div>
-        <h1>the nav</h1>
-     
+        <Nav tabs>
+          <NavItem>
+            <NavLink href="#" active>Link</NavLink>
+          </NavItem>
+          <Dropdown nav isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+            <DropdownToggle nav caret>
+              Dropdown
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem header>Header</DropdownItem>
+              <DropdownItem disabled>Action</DropdownItem>
+              <DropdownItem>Another Action</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Another Action</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+          <NavItem>
+            <NavLink href="#">Link</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">Another Link</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink disabled href="#">Disabled Link</NavLink>
+          </NavItem>
+        </Nav>
       </div>
     );
   }
 }
-
-export default Nav;
