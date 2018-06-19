cfd52bd85f70f552741ee9e37f32c7a19db0bd86
Chris Gonzales <cm.gonzales@yahoo.com>
2018-06-18 17:57:02 -0700

    added ui

--- src/text.jsx
+++ src/text.jsx
@@ -1,22 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-function activate(e){
+export default class text extends React.Component {
 
-console.log("clicked");
-
-}
-
-class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+ handleSubmit(event) {
+    console.log(this.state.value)
+  }
+    render() {
     return (
-      <div>
-     <input type="text" value= {this.state.value} onChange={this.handleChange}/>
-     <button onClick={activate}>sumbit</button>
-       
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
 
-export default App;
 
